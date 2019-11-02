258d69063ca907880249cf6fd69597b51d07cfe7
simov <simeonvelichkov@gmail.com>
2019-11-02 17:31:03 +0200

    Refactor log methods

--- logs.js
+++ logs.js
@@ -9,35 +9,42 @@ var debug = process.env.DEBUG.split(',')
   .reduce((debug, key) => (debug[key] = true, debug), {})
 
 
+var header = (name) =>
+  /req/.test(name) ? c.cyan.inverse(name) :
+  /res/.test(name) ? c.yellow.inverse(name) :
+  /json|body|form/.test(name) ? c.gray.inverse(name) :
+  null
+
+
 var method = (verb) =>
-  /GET/.test(verb) ? c.green :
-  /POST|PUT/.test(verb) ? c.cyan :
-  /DELETE/.test(verb) ? c.red :
-  /HEAD|OPTIONS|CONNECT/.test(verb) ? c.yellow :
-  /TRACE/.test(verb) ? c.gray :
-  c.gray
+  /GET/.test(verb) ? c.green(verb) :
+  /POST|PUT/.test(verb) ? c.cyan(verb) :
+  /DELETE/.test(verb) ? c.red(verb) :
+  /HEAD|OPTIONS|CONNECT/.test(verb) ? c.yellow(verb) :
+  /TRACE/.test(verb) ? c.gray(verb) :
+  null
 
 
-var uri = ({protocol, hostname, port, path}) => [
+var status = (code, message) =>
+  /^1/.test(code) ? c.white(`${code} ${message}`) :
+  /^2/.test(code) ? c.green(`${code} ${message}`) :
+  /^3/.test(code) ? c.yellow(`${code} ${message}`) :
+  /^4/.test(code) ? c.red(`${code} ${message}`) :
+  /^5/.test(code) ? c.red.bold(`${code} ${message}`) :
+  null
+
+
+var url = ({protocol, hostname, port, path}) => c.yellow([
   `${protocol}//`,
   hostname,
   port ? `:${port}` : '',
   path || '/',
-].join('')
+].join(''))
 
 
-var status = (code) =>
-  /^1/.test(code) ? c.white :
-  /^2/.test(code) ? c.green :
-  /^3/.test(code) ? c.yellow :
-  /^4/.test(code) ? c.red :
-  /^5/.test(code) ? c.red.bold :
-  c.white
-
-
-var prettyjson = (obj) =>
+var prettyjson = (json) =>
   j.render(
-    obj,
+    json,
     debug.nocolor ? {noColor: true} : {keysColor: 'blue', stringColor: 'grey'},
     4
   )
@@ -46,9 +53,9 @@ var prettyjson = (obj) =>
 var request = ({req, body, options}) => {
   // req method url
   console.log(
-    c.cyan.inverse('req'),
-    method(options.method)(options.method),
-    c.yellow(uri(options)),
+    header('req'),
+    method(options.method),
+    url(options),
   )
   // headers
   console.log(prettyjson(
@@ -59,19 +66,19 @@ var request = ({req, body, options}) => {
   ))
   // body
   if (debug.body && body) {
-    console.log(c.gray.inverse('body'))
+    console.log(header('body'))
     console.log(body)
   }
   // json or querystring
   if (debug.json && body) {
-    var header = Object.keys(options.headers)
+    var name = Object.keys(options.headers)
       .find((name) => name.toLowerCase() === 'content-type')
-    if (/application\/json/.test(options.headers[header])) {
-      console.log(c.gray.inverse('json'))
+    if (/application\/json/.test(options.headers[name])) {
+      console.log(header('json'))
       console.log(prettyjson(JSON.parse(body)))
     }
     else if (/application\/x-www-form-urlencoded/.test(options.headers[header])) {
-      console.log(c.gray.inverse('form'))
+      console.log(header('form'))
       console.log(prettyjson(qs.parse(body)))
     }
   }
@@ -82,8 +89,8 @@ var response = ({res, body, json}) => {
   if (res) {
     // res status message
     console.log(
-      c.yellow.inverse('res'),
-      status(res.statusCode)(res.statusCode + ' ' + res.statusMessage)
+      header('res'),
+      status(res.statusCode, res.statusMessage)
     )
     // headers
     console.log(
@@ -92,12 +99,12 @@ var response = ({res, body, json}) => {
   }
   // body
   if (body) {
-    console.log(c.gray.inverse('body'))
+    console.log(header('body'))
     console.log(body)
   }
   // json
   if (json) {
-    console.log(c.gray.inverse('json'))
+    console.log(header('json'))
     console.log(prettyjson(json))
   }
 }
